4b363ac51a5615147a332052be86811b84ecefcb
Muhammad Kamran <mkamran1967@gmail.com>
2021-03-31 10:18:17 -0700

    $1

---
+++ frontend/components/SingleProduct.js
@@ -0,0 +1,72 @@
+import { useQuery } from '@apollo/client';
+import gql from 'graphql-tag';
+import Head from 'next/head';
+import styled from 'styled-components';
+import DisplayError from './ErrorMessage';
+
+const SINGLE_ITEM_QUERY = gql`
+  query SINGLE_ITEM_QUERY($id: ID!) {
+    Product(where: { id: $id }) {
+      id
+      name
+      price
+      description
+      photo {
+        altText
+        image {
+          publicUrlTransformed
+        }
+      }
+    }
+  }
+`;
+
+const ProductStyles = styled.div`
+  display: grid;
+  grid-auto-columns: 1fr;
+  grid-auto-flow: column;
+  min-width: var(--maxWidth);
+  justify-content: center;
+  align-items: top;
+  gap: 2rem;
+
+  img {
+    width: 100%;
+    object-fit: contain;
+  }
+`;
+
+export default function SingleProduct({ id }) {
+  const { data, loading, error } = useQuery(SINGLE_ITEM_QUERY, {
+    variables: {
+      id,
+    },
+  });
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <DisplayError error={error} />;
+
+  const {
+    Product: {
+      name,
+      description,
+      price,
+      photo: {
+        altText,
+        image: { publicUrlTransformed },
+      },
+    },
+  } = data;
+  return (
+    <ProductStyles>
+      <Head>
+        <title>Flea Market | {name}</title>
+      </Head>
+      <img src={publicUrlTransformed} alt={altText} />
+      <div className="details">
+        <h2>{name}</h2>
+        <p>{description}</p>
+      </div>
+    </ProductStyles>
+  );
+}
